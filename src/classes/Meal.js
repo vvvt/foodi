ae484cbac1f3870f2e2a6497b2952bdb59fe366e
Louis Wilke <louiswilke@gmx.de>
2019-11-10 14:26:23 +0100

    fixed missing property canteenId

--- src/classes/Meal.js
+++ src/classes/Meal.js
@@ -1,5 +1,5 @@
 /** @typedef {{ id: number, name: string, notes: string[], prices: { [priceGroup: string]: number }, category: string }} MealObj */
-/** @typedef {{ id: number, name: string, date: string, notes: string, prices: string, category: string }} MealDatabaseRow */
+/** @typedef {{ id: number, canteenId: number, name: string, date: string, notes: string, prices: string, category: string }} MealDatabaseRow */
 
 export default class Meal {
 
@@ -14,7 +14,7 @@ export default class Meal {
      * @param {string[]} notes The notes eg. "vegetarian"
      */
     constructor( id, canteenId, name, date, category, prices = {}, notes = [] ) {
-        if (!Meal.isValidMealDate(date)) throw new Error('Invalid date format! Must be of format "YYYY-MM-DD".');
+        if (!Meal.isValidMealDate(date)) throw new Error(`Invalid date format! Must be of format "YYYY-MM-DD" but was "${date}"`);
 
         this.id = id;
         this.canteenId = canteenId;
@@ -28,10 +28,11 @@ export default class Meal {
     /**
      * Creates a meal instance from a given object and date
      * @param {MealObj} obj The object containing all meal properties as returned from the OpenMensa API
+     * @param {number} canteenId The id of the canteen that this meal belongs to
      * @param {string} date The date this meals is in the canteen
      */
-    static fromObject( obj, date ) {
-        return new Meal(obj.id, obj.name, date, obj.category, obj.prices, obj.notes);
+    static fromObject( obj, canteenId, date ) {
+        return new Meal(obj.id, canteenId, obj.name, date, obj.category, obj.prices, obj.notes);
     }
 
     /**
@@ -39,7 +40,7 @@ export default class Meal {
      * @param {MealDatabaseRow} row The result row as loaded from the database
      */
     static fromDatabase( row ) {
-        return new Meal( row.id, row.name, row.date, row.category, JSON.parse(`[${row.prices}]`), {} );
+        return new Meal( row.id, row.canteenId, row.name, row.date, row.category, JSON.parse(`[${row.prices}]`), {} );
     }
 
     /**
