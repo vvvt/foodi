df38518eca868cfa0b796cb1fa23a6ec193a806f
Vincent Thiele <hey@thielegram.de>
2019-11-12 14:10:21 +0100

    ADD screens

---
+++ src/screens/MapScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default class MapScreen extends React.Component {
+    
+    render() {
+        return (
+            <View>
+                <Text>MapScreen</Text>
+            </View>
+        );
+    }
+}
---
+++ src/screens/SettingsScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default class SettingsScreen extends React.Component {
+    
+    render() {
+        return (
+            <View>
+                <Text>SettingsScreen</Text>
+            </View>
+        );
+    }
+}
