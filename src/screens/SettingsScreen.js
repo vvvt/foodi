6fe23c66dd2457065e1c678c9e3c58669db6213d
Vincent Thiele <hey@thielegram.de>
2019-11-13 14:14:47 +0100

    ADD basic settings screen

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,13 +1,118 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, FlatList, Picker, StyleSheet, SafeAreaView, Switch } from "react-native";
+import Constants from 'expo-constants';
+
+const DATA = [
+  {
+    id: "sold-out",
+    title: "Show sold out meals"
+  },
+  {
+    id: "fetch-week",
+    title: "Fetch week's menu via",
+    options: [
+      {
+        label: "WiFi only",
+        value: "wifi"
+      },
+      {
+        label: "WiFi/Cellular",
+        value: "data"
+      }
+    ]
+  },
+  {
+    id: "fetch-images",
+    title: "Fetch meal images via",
+    options: [
+      {
+        label: "WiFi only",
+        value: "wifi"
+      },
+      {
+        label: "WiFi/Cellular",
+        value: "data"
+      }
+    ]
+  }
+
+];
+
+function ToggleItem({ title }) {
+  const [toggleValue, setToggleValue] = useState(0);
+
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+      <Switch
+        value={toggleValue}
+        onValueChange={(val) => {
+          setToggleValue(val);
+          alert(title + " " + val);
+        }}
+      />
+    </View>
+  );
+}
+
+function SelectItem({ title, options }) {
+  const [selectValue, setSelectValue] = useState("wifi");
+
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+      <Picker
+          style={styles.picker}
+          selectedValue={selectValue}
+          onValueChange={(itemValue) =>
+            {
+              alert(title + " " + itemValue);
+              setSelectValue(itemValue);
+            }
+          }
+      >
+        {options.map((v) => {
+          return <Picker.Item label={v.label} value={v.value} />
+        })}
+      </Picker>
+        </View >
+    )
+}
 
 export default class SettingsScreen extends React.Component {
-    
-    render() {
-        return (
-            <View>
-                <Text>SettingsScreen</Text>
-            </View>
-        );
-    }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={DATA}
+          renderItem={({ item }) => item.options ? <SelectItem title={item.title} options={item.options} /> : <ToggleItem title={item.title} />}
+        />
+      </SafeAreaView>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+    marginHorizontal: 16,
+  },
+  item: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#f9f9f9',
+    padding: 16,
+    marginVertical: 2,
+  },
+  title: {
+    fontSize: 16,
+  },
+  picker: {
+    height: 60,
+    width: 200
+  }
+  
+});
