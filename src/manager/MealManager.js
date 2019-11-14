c2d6ac622a2b1b6d586cb67f51d5eff00846fe3e
Louis Wilke <louiswilke@gmx.de>
2019-11-14 14:24:11 +0100

    moved canteen management into an own class

--- src/manager/MealManager.js
+++ src/manager/MealManager.js
@@ -2,8 +2,6 @@ import NetworkManager from "./NetworkManager";
 import DatabaseManager from "./DatabaseManager";
 
 import Meal from "../classes/Meal";
-import Canteen from "../classes/Canteen";
-import Coordinate from "../classes/Coordinate";
 
 const networkManager = NetworkManager.instance;
 const databaseManager = DatabaseManager.instance;
@@ -35,27 +33,6 @@ export default class MealManager {
         
         // add date property
         return mealsObjs.map( m => Meal.fromObject( m, canteenId, day ) );
-    }
-
-    /**
-     * Fetches canteen in a given radius around a given position
-     * @param {Coordinate} position The position to find canteens from
-     * @param distance Default: 7.5. The maximum distance in km to the given position a returned canteen can have
-     */
-    async fetchCanteens( position, distance = 7.5 ) {
-        if (!Array.isArray(position) || position.length !== 2) throw new TypeError("The position must be an array containing 2 elements!");
-
-        /** @type {import("../classes/Canteen").CanteenObj[]} */
-        const canteenObjs = await networkManager.fetchWithParams(
-            NetworkManager.ENDPOINTS.OPEN_MENSA_API + `/canteens`,
-            {
-                "near[lat]": position[0],
-                "near[lng]": position[1],
-                "near[dist]": distance
-            }
-        );
-
-        return canteenObjs.map( c => Canteen.fromObject( c ) );
     }
 
     /**
@@ -102,44 +79,6 @@ export default class MealManager {
             })
         );
 
-    }
-
-    /**
-     * Saves the given array of canteens in the database
-     * @param {Canteen[]} canteens The canteens to save
-     */
-    async saveCanteens( canteens ) {
-        await databaseManager.runInTransaction(
-            canteens.map( c =>
-                [DatabaseManager.STATEMENTS.INSERT_INTO_CANTEENS, c.id, c.name, c.city, c.address, c.coordinate.latitude, c.coordinate.longitude]
-            )
-        );
-    }
-
-    /**
-     * Loads all cached (persisted) canteens from the database. A serach region can be passed
-     * @param {Coordinate} position Optional: The position to load cached canteens from.
-     * If none given, all canteens will be returned
-     * @param distance Default: 7.5. The search radius around the given position in km 
-     */
-    async loadCanteens( position = null, distance = 7.5 ) {
-        /** @type {import("../classes/Canteen").CanteenDatabaseRow[]} */
-        let rows = [];
-
-        if (position == null) {
-            rows = await databaseManager.getAll(DatabaseManager.STATEMENTS.LOAD_ALL_CANTEENS);
-        } else {
-            const boundingBox = Coordinate.getBoundingCoordinates( position, distance );
-            rows = await databaseManager.getAll(
-                DatabaseManager.STATEMENTS.LOAD_CANTEENS_BETWEEN_COORDINATES,
-                [boundingBox[0].latitude, boundingBox[1].latitude, boundingBox[0].longitude, boundingBox[1].longitude]
-            );
-
-            // sadly SQLite does not have any SQRT() function, so we have to do further filtering here
-            rows = rows.filter( r => Coordinate.calcDistance( Coordinate.fromDatabase(r), position ) <= distance );
-        }
-
-        return rows.map( r => Canteen.fromDatabase(r) );
     }
 
 }
