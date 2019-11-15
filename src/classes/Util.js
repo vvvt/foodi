1c7db59f27a63ae6f9e3032d38ef171d47601de9
Louis Wilke <louiswilke@gmx.de>
2019-11-15 13:43:58 +0100

    added a unix timestamp function to Util

--- src/classes/Util.js
+++ src/classes/Util.js
@@ -1,3 +1,10 @@
 export default class Util {
-    
+
+    /**
+     * The current unix timestamp
+     */
+    static get currentUnixTimestamp() {
+        return Math.round( Date.now()/1000 );
+    }
+
 }
