9314361b34ce2e6af1b76c17d51754f1d3b30e36
Vincent Thiele <hey@thielegram.de>
2019-12-03 12:22:05 +0100

    ADD test function

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { FlatList, SafeAreaView } from "react-native";
 import ToggleItem from "../components/ToggleItem";
-import DATA from "../classes/Settings";
+import Settings from "../classes/Settings";
 import styles from "./TemplateScreen.css";
+import SettingsManager from "../manager/SettingsManager";
+
+const settingsManager = SettingsManager.instance;
 
 export default class SettingsScreen extends React.Component {
 
   render() {
+
+    Settings.forEach((item) => {
+      settingsManager.storeSetting(item.id, "false");
+    });
+    console.log(settingsManager.getMultipleSettings(Settings.map((item) => item.id), Settings.map(() => "boolean")));
+
     return (
       <SafeAreaView style={styles.container}>
         <FlatList
-          data={DATA}
+          data={Settings}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => <ToggleItem title={item.title} id={item.id} />}
         />
