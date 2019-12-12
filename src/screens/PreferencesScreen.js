5a41f0a87cbb8c5caaa2a8aef7c28f5b1483e009
Louis Wilke <louiswilke@gmx.de>
2019-12-12 22:09:03 +0100

    avoided the use of forceUpdate()

--- src/screens/PreferencesScreen.js
+++ src/screens/PreferencesScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, SectionList, SafeAreaView } from "react-native";
+import { Text, SectionList, SafeAreaView, View, ActivityIndicator } from "react-native";
 import ToggleItem from "../components/ToggleItem";
 import PREFERENCES from "../classes/Preferences";
 import styles from "./TemplateScreen.css";
@@ -9,7 +9,38 @@ const settingsManager = SettingsManager.instance;
 
 export default class PreferencesScreen extends React.Component {
 
+  state = {
+    loading: true
+  }
+
+  // the function below is called before the screen is opened for the first time (it removes this listener by itself)
+  initialSettingsLoader = this.props.navigation.addListener(
+    "willFocus",
+    () => {
+
+      // load settings from settingsmanager into state
+      const state = { loading: false };
+      PREFERENCES.forEach( preference =>
+        preference.data.forEach( data => {
+          state[data.id] = settingsManager.hasSetting(data.id) ? settingsManager.getSetting(data.id).value : data.default;
+        })
+      );
+      this.setState(state);
+
+      // remove listener
+      this.initialSettingsLoader.remove();
+      delete this.initialSettingsLoader;
+
+    }
+  )
+
   render() {
+
+    if (this.state.loading) return (
+      <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }} >
+        <ActivityIndicator size="large" />
+      </View>
+    )
 
     return (
       <SafeAreaView style={styles.container}>
@@ -21,9 +52,9 @@ export default class PreferencesScreen extends React.Component {
               title={item.name}
               onValueChange={value => {
                 settingsManager.storeSetting(new Setting(item.id, value));
-                this.forceUpdate();
+                this.setState({ [item.id]: value });
               }}
-              toggleValue={settingsManager.hasSetting(item.id) ? settingsManager.getSetting(item.id).value : item.default}
+              toggleValue={this.state[item.id]}
             />
           )}
           renderSectionHeader={({ section: { title } }) => (
--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlatList, SafeAreaView } from "react-native";
+import { FlatList, SafeAreaView, View, ActivityIndicator } from "react-native";
 import ToggleItem from "../components/ToggleItem";
 import SETTINGS from "../classes/Settings";
 import styles from "./TemplateScreen.css";
@@ -7,9 +7,38 @@ import SettingsManager, { Setting } from "../manager/SettingsManager";
 
 const settingsManager = SettingsManager.instance;
 
-export default class SettingsScreen extends React.Component {
+export default class SettingsScreen extends React.PureComponent {
+
+  state = {
+    loading: true
+  }
+
+  // the function below is called before the screen is opened for the first time (it removes this listener by itself)
+  initialSettingsLoader = this.props.navigation.addListener(
+    "willFocus",
+    () => {
+
+      // load settings from settingsmanager into state
+      const state = { loading: false };
+      SETTINGS.forEach( setting => {
+        state[setting.id] = settingsManager.hasSetting(setting.id) ? settingsManager.getSetting(setting.id).value : setting.default;
+      });
+      this.setState(state);
+
+      // remove listener
+      this.initialSettingsLoader.remove();
+      delete this.initialSettingsLoader;
+
+    }
+  )
 
   render() {
+
+    if (this.state.loading) return (
+      <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }} >
+        <ActivityIndicator size="large" />
+      </View>
+    )
 
     return (
       <SafeAreaView style={styles.container}>
@@ -20,10 +49,11 @@ export default class SettingsScreen extends React.Component {
             <ToggleItem
               title={item.title}
               onValueChange={value => {
+                console.log("store " + item.id + " " + value);
                 settingsManager.storeSetting( new Setting(item.id, value) );
-                this.forceUpdate();
+                this.setState({ [item.id]: value });
               }}
-              toggleValue={settingsManager.hasSetting(item.id) ? settingsManager.getSetting(item.id).value : item.default}
+              toggleValue={this.state[item.id]}
             />
           }
         />
