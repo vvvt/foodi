bd3f03db995ea25e99abc0b405ae64121fe9016f
Vincent Thiele <hey@thielegram.de>
2020-01-18 17:17:11 +0100

    ADD in basic canteen view

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -8,34 +8,59 @@ import Util from "../classes/Util";
 /** @typedef {import("../classes/Canteen").default} Canteen */
 
 export default class MealItem extends React.PureComponent {
+  /** @type {{ meal: Meal, canteen: Canteen, distance: number, view: string, OnItemPressed?: VoidFunction }} */
+  props;
 
-    /** @type {{ meal: Meal, canteen: Canteen, distance: number, OnItemPressed?: VoidFunction }} */
-    props;
+  static defaultProps = {
+    OnItemPressed: () => {}
+  };
 
-    static defaultProps = {
-        OnItemPressed: () => {}
-    };
+  render() {
+    const { meal, canteen, distance, view } = this.props;
+    const mealPrice = meal.prices.Studierende
+      ? meal.prices.Studierende.toFixed(2) + "€"
+      : "n/a";
 
-    render() {
-
-        const { meal, canteen, distance } = this.props;
-        const mealPrice = meal.prices.Studierende ? meal.prices.Studierende.toFixed(2) + "€" : "n/a";
-
-        return (
-            <TouchableOpacity onPress={this.props.OnItemPressed} >
-                <View style={[styles.item, { backgroundColor: meal.isEveningMeal ? "lightblue" : undefined }]}>
-                    {meal.isEveningMeal ? <Icon color="yellow" name="md-moon" style={styles.moonIcon} size={25} /> : null}
-                    <View style={styles.row}>
-                        <Text style={[styles.cardTitle, styles.columnLeft]}>{meal.name}</Text>
-                        <Text style={[styles.cardTitle, styles.columnRight]}>{mealPrice}</Text>
-                    </View>
-                    <View style={styles.smallSpacerVertical} />
-                    <View style={styles.row}>
-                        <Text style={[styles.cardSubTitle, styles.columnLeft]}>{canteen.name}</Text>
-                        <Text numberOfLines={1} adjustsFontSizeToFit style={[styles.cardSubTitle, styles.columnRight]}>{Util.distanceToString(distance)}</Text>
-                    </View>
-                </View>
-            </TouchableOpacity>
-        );
-    }
+    return (
+      <TouchableOpacity onPress={this.props.OnItemPressed}>
+        <View
+          style={[
+            styles.item,
+            { backgroundColor: meal.isEveningMeal ? "lightblue" : undefined },
+            { borderColor: meal.isEveningMeal ? "lightblue" : "#ddd" }
+          ]}
+        >
+          {meal.isEveningMeal ? (
+            <Icon
+              color="yellow"
+              name="md-moon"
+              style={styles.moonIcon}
+              size={25}
+            />
+          ) : null}
+          <View style={styles.row}>
+            <Text style={[styles.cardTitle, styles.columnLeft]}>
+              {meal.name}
+            </Text>
+            <Text style={[styles.cardTitle, styles.columnRight]}>
+              {mealPrice}
+            </Text>
+          </View>
+          <View style={styles.smallSpacerVertical} />
+          <View style={styles.row}>
+            <Text style={[styles.cardSubTitle, styles.columnLeft]}>
+              {view == "outside" ? canteen.name : meal.category}
+            </Text>
+            <Text
+              numberOfLines={1}
+              adjustsFontSizeToFit
+              style={[styles.cardSubTitle, styles.columnRight]}
+            >
+              {view == "outside" ? Util.distanceToString(distance) : ""}
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  }
 }
