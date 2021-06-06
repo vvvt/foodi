a1bdb38d1b6dec47687f4160025a31f7cdaf2b56
Louis Wilke <louiswilke@gmx.de>
2021-06-06 11:31:36 +0200

    removed deprecated AsyncStorage package

--- src/manager/SettingsManager.js
+++ src/manager/SettingsManager.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import SETTINGS from "../classes/Settings";
 import FILTERS from "../classes/Filters";
 
